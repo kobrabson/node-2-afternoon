2567b684f121b13a909404f3f1eff4c13335c98e
kobrabson <67922399+kobrabson@users.noreply.github.com>
2020-07-28 16:03:17 -0600

    4 - 6 done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-const express = require ('express')
+const express = require ('express');
+const mc = require('./controllers/message_controller');
 const app = express();
+
 app.use(express.json())
 
-const port = 4200
+
+const messagesBaseUrl = "/api/messages";
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+const port = 3001
 
 app.listen(port, () => {
     console.log(`running on port ${port}`)
